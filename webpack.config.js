b740ad5501ac952e792eee0bb74228a98ee25c91
“Sam <sam.alsmadi@gmail.com>
2017-11-21 11:37:16 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  devtool: 'source-map'
+
+}
